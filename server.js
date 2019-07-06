e1048d93d635b7cd7ab6792db96cf7fc8e576ca9
David Norris <dnorris@luf.co>
2019-07-06 16:12:52 -0400

    finished components and routing

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
 const schema = require('./schema')
+// For deploy
+// const path = require('path')
 
 const app = express();
+
+// Allow cross-origin
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
+
+// for deploy
+// app.use(express.static('public'))
+
+// For deploy - looking for index.html in public folder any time route is hit
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// })
 
 const PORT = process.env.PORT || 5000
 
